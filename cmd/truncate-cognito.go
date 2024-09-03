258e95665180ccaf755590cb9a682a0da735a596
Qosdil A <qosdil@yahoo.com>
2024-09-03 11:25:50 +0700

    Improved outputs of truncate-cognito command.

--- cmd/truncate-cognito.go
+++ cmd/truncate-cognito.go
@@ -37,13 +37,14 @@ to quickly create a Cobra application.`,
 			log.Fatalf("error on getting a Cognito client: %s", err.Error())
 		}
 
+		log.Printf("start truncating Cognito user pool %s...", cognito.Config.PoolID)
 		numAffected, err := cognito.Truncate(ctx)
 		if err != nil {
 			log.Fatalf("error on truncating Cognito user pool: %s", err.Error())
 		}
 
 		if numAffected > 0 {
-			fmt.Printf("successfully deleted %d Cognito user(s)\n", numAffected)
+			log.Printf("successfully deleted %d Cognito user(s)\n", numAffected)
 		} else {
 			fmt.Println("user pool is empty")
 		}
