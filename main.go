fca5cc41699e3d81c82d15686283f07434143f7c
Qosdil A <qosdil@yahoo.com>
2024-09-27 17:06:14 +0700

    Remove the constraint of .env file existence for easier deployment on server.

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"log"
 	"tt-fraudsters-suspender/cmd"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error loading .env file")
-	}
+	godotenv.Load()
 	cmd.Execute()
 }
